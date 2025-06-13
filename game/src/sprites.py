3716084ee800afc18999c5c57be63222894df134
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-06-12 23:28:30 -0800

    feat: Adding a sprite class for display images.

---
+++ game/src/sprites.py
@@ -0,0 +1,8 @@
+from  settings import *
+
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self,pos,surf,groups):
+        super().__init__(groups)
+        self.image = surf
+        # frect() => floating point rectangle
+        self.rect = self.image.get_frect(topleft=pos)
