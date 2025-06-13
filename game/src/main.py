bb708bf7f5d0fe454dc13fdb0a80b275ba5688cb
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-06-12 23:00:43 -0800

    feat: Importing tmx maps.

--- game/src/main.py
+++ game/src/main.py
@@ -1,10 +1,18 @@
 from settings import *
+from pytmx.util_pygame import load_pygame
+from  os.path import join,exists
 
 class Game:
     def __init__(self):
         pygame.init()
         self.display = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption("Monster Bash")
+        self.import_assets()
+
+    def import_assets(self):
+        print(exists("../assets/maps/world.tmx"))
+        self.tmx_maps = {'world': load_pygame(join("..", "assets", "maps", "world.tmx"))}
+        print(self.tmx_maps)
 
     def run(self):
         while True:
