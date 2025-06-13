37630714df52635e43eafd33cb0d046de275126f
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-06-13 13:44:53 -0800

    style: code formatting.

--- game/src/main.py
+++ game/src/main.py
@@ -1,9 +1,10 @@
 from settings import *
 from pytmx.util_pygame import load_pygame
-from  os.path import join,exists
+from os.path import join, exists
 
-from  sprites import Sprite
-from  entities import Player
+from sprites import Sprite
+from entities import Player
+
 
 class Game:
     def __init__(self):
@@ -14,28 +15,28 @@ class Game:
         self.all_sprites = pygame.sprite.Group()
 
         self.import_assets()
-        self.setup(self.tmx_maps["world"],"house")
+        self.setup(self.tmx_maps["world"], "house")
 
     def import_assets(self):
         # print(exists("../assets/maps/world.tmx"))
         self.tmx_maps = {'world': load_pygame(join("..", "assets", "maps", "world.tmx"))}
         # print(self.tmx_maps)
 
-    def setup(self,tmx_map,player_start_pos):
+    def setup(self, tmx_map, player_start_pos):
         terrain_layer_tiles = tmx_map.get_layer_by_name("Terrain").tiles()
-        for x,y,surf in terrain_layer_tiles:
+        for x, y, surf in terrain_layer_tiles:
             position = (x * TILE_SIZE, y * TILE_SIZE)
-            Sprite(position, surf,self.all_sprites)
+            Sprite(position, surf, self.all_sprites)
 
         entities_layer_tiles = tmx_map.get_layer_by_name("Entities")
         for obj in entities_layer_tiles:
-            if obj.name =="Player" and obj.properties['pos']==player_start_pos:
+            if obj.name == "Player" and obj.properties['pos'] == player_start_pos:
                 player_pos = (obj.x, obj.y)
                 Player(player_pos, self.all_sprites)
 
     def run(self):
         while True:
-            #event loop
+            # event loop
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -45,6 +46,7 @@ class Game:
             self.all_sprites.draw(self.display)
             pygame.display.update()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     game = Game()
     game.run()
