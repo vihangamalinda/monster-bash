91073baaed8641128069b61c3e632879659819ac
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-06-15 01:54:40 -0800

    feat: Importing assest as a dictionary.

--- game/src/support.py
+++ game/src/support.py
@@ -20,3 +20,13 @@ def import_folder(*path):
             surface = pygame.image.load(full_path).convert_alpha()
             frames.append(surface)
     return frames
+
+def import_folder_dic(*path):
+    frames ={}
+    for folder_path,sub_folders,image_names in walk(join(*path)):
+        for img_name_with_extension in image_names:
+            full_path = join(folder_path,img_name_with_extension)
+            surface = pygame.image.load(full_path).convert_alpha()
+            image_name = img_name_with_extension.split(".")[0]
+            frames[image_name] = surface
+    return frames
