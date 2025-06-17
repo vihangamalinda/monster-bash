57175e4d67213ec18df07f28bdb93c5861b82507
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-06-17 03:12:23 -0800

    feat: Importing All characters to a dictionary.

--- game/src/support.py
+++ game/src/support.py
@@ -90,3 +90,14 @@ def character_importer(cols,rows,*path):
         new_dict[direction] = [frame_dic[(col,row)] for col in range(cols)]
         new_dict[f"{direction}_idle"]=[frame_dic[(0,row)]]
     return new_dict
+
+
+def all_character_import(*path):
+    new_dict ={}
+    print("HGI")
+    for folder_path, sub_folders, image_names in walk(join(*path)):
+        for img_name_with_extension in image_names:
+            img_name = img_name_with_extension.split(".")[0]
+            new_dict[img_name] = character_importer(4,4,*path,img_name)
+            print(img_name)
+    return new_dict
