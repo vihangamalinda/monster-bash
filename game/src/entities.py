7f945a1cf4be00920b705fe672842fcb6df487ce
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-06-17 03:21:16 -0800

    feat: Player will inherit the Entity class.

--- game/src/entities.py
+++ game/src/entities.py
@@ -2,10 +2,14 @@ import pygame
 
 from settings import *
 
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups):
+class Entity(pygame.sprite.Sprite):
+    def __init__(self,pos,frames,groups):
         super().__init__(groups)
+
+
+class Player(Entity):
+    def __init__(self, pos,frames, groups):
+        super().__init__(pos,frames,groups)
         self.image = pygame.Surface((100, 100))
         self.image.fill("red")
         self.rect = self.image.get_frect(center=pos)
