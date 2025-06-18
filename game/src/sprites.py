be3a2162b97584f37ffff2f11f78592a6fb3d6f6
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-06-17 23:05:43 -0800

    feat:Injecting the drawing priority to Sprite and AnimatedSprite class.

--- game/src/sprites.py
+++ game/src/sprites.py
@@ -2,18 +2,19 @@ from settings import *
 
 
 class Sprite(pygame.sprite.Sprite):
-    def __init__(self, pos, surf, groups):
+    def __init__(self, pos, surf, groups,drawing_priority=WORLD_PRIORITY_ORDER["main"]):
         super().__init__(groups)
         self.image = surf
         # frect() => floating point rectangle
         self.rect = self.image.get_frect(topleft=pos)
+        self.drawing_priority = drawing_priority
 
 
 class AnimatedSprite(Sprite):
-    def __init__(self, pos, frames, groups):
+    def __init__(self, pos, frames, groups,drawing_priority=WORLD_PRIORITY_ORDER["main"]):
         self.frame_index = 0
         self.frames = frames
-        super().__init__(pos, frames[0], groups)
+        super().__init__(pos, frames[0], groups,drawing_priority)
 
     def animate(self, dt):
         self.frame_index += ANIMATION_SPEED * dt
