ba307f2acf057d1ab452e7585accfcf33a4d83f9
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-06-17 23:37:58 -0800

    feat:Differentiating each sprite according to their drawing-priority value and drawing them according to that priority.

--- game/src/groups.py
+++ game/src/groups.py
@@ -10,5 +10,11 @@ class AllSprites(pygame.sprite.Group):
     def draw(self, player_center):
         self.offset.x = -(player_center[0] - WINDOW_WIDTH / 2)
         self.offset.y = -(player_center[1] - WINDOW_HEIGHT / 2)
-        for sprite in self:
-            self.display_surface.blit(sprite.image, sprite.rect.topleft + self.offset)
+
+        background_sprites =[sprite for sprite in self if sprite.drawing_priority <WORLD_PRIORITY_ORDER["main"]]
+        main_sprites =[sprite for sprite in self if sprite.drawing_priority == WORLD_PRIORITY_ORDER["main"]]
+        foreground_sprites =[sprite for sprite in self if sprite.drawing_priority > WORLD_PRIORITY_ORDER["main"]]
+
+        for layer in (background_sprites, main_sprites, foreground_sprites):
+            for sprite in layer:
+                self.display_surface.blit(sprite.image, sprite.rect.topleft + self.offset)
